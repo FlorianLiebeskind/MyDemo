aa4fa03d59951a3c9e948db76c2148b20b66ad0a
Liebeskind <liebeskind@live.com>
2020-12-16 22:25:50 +0800

    添加项目文件。

---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class cell 
+        { 
+            public int a { get; set; }
+            public int b { get; set; }
+            public bool 置位 { get; set; }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IList<cell> cells = new BindingList<cell>();
+            for (int i = 0; i < 10; i++)
+            {
+                cells.Add(new cell() { a = i, b = i, 置位 = false });
+            }
+
+
+            this.dataGridView1.DataSource = cells;
+
+
+
+            dataGridView1[0, 0].Style.BackColor = Color.Pink;
+
+
+
+            ////////////////////
+
+            DataGridViewButtonColumn dgv_button_col = new DataGridViewButtonColumn();
+
+            // 设定列的名字
+            dgv_button_col.Name = "Detail";
+
+            // 在所有按钮上表示"查看详情"
+            dgv_button_col.UseColumnTextForButtonValue = true;
+            dgv_button_col.Text = "查看详情";
+
+            // 设置列标题
+            dgv_button_col.HeaderText = "详情";
+
+            // 向DataGridView追加
+            dataGridView1.Columns.Insert(dataGridView1.Columns.Count, dgv_button_col);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.Columns[e.ColumnIndex].Name == "Detail")
+            {
+                MessageBox.Show("行: " + e.RowIndex.ToString() + ", 列: " + e.ColumnIndex.ToString() + "; 被点击了");
+            }
+        }
+    }
+}
